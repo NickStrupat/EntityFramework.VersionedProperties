dfbed041b7a3b5f022df9be0f5b40c4ee758cf64
Nick Strupat <nick.strupat@gmail.com>
2016-10-24 18:43:25 -0400

    class constraint on TValue in RequiredValueVersionedBase

--- EntityFrameworkCore.VersionedProperties/VersionedTypes/BaseTypes/RequiredValueVersionedBase.cs
+++ EntityFrameworkCore.VersionedProperties/VersionedTypes/BaseTypes/RequiredValueVersionedBase.cs
@@ -5,7 +5,9 @@ namespace EntityFrameworkCore.VersionedProperties {
 #else
 namespace EntityFramework.VersionedProperties {
 #endif
-	public abstract class RequiredValueVersionedBase<TValue, TVersion, TIVersionedTypes> : VersionedBase<TValue, TVersion, TIVersionedTypes> where TVersion : VersionBase<TValue>, new() {
+	public abstract class RequiredValueVersionedBase<TValue, TVersion, TIVersionedTypes> : VersionedBase<TValue, TVersion, TIVersionedTypes>
+	where TValue : class
+	where TVersion : VersionBase<TValue>, new() {
 		[Required]
 		public sealed override TValue Value {
 			get { return base.Value; }
