85e2566c2e1223cd4136b14961c2495524886bd0
Nick Strupat <nick.strupat@gmail.com>
2016-11-25 01:20:18 -0500

    starting new test code

---
+++ TestsCore/TestBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+#if NET40
+using NUnit.Framework;
+using Fact = NUnit.Framework.TestAttribute;
+#else
+using Xunit;
+#endif
+
+#if EF_CORE
+using Microsoft.EntityFrameworkCore;
+namespace EntityFrameworkCore.VersionedProperties.Tests {
+#else
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+namespace EntityFramework.VersionedProperties.Tests {
+#endif
+	public abstract class TestBase : IDisposable {
+		protected virtual void Setup() {}
+		protected virtual void Teardown() {}
+
+		protected abstract void TestImpl();
+
+		[Fact]
+		public void Test() {
+			if (!semaphoreSlim.Wait(10000))
+				Assert.True(false, "Wait failed due to timeout");
+			Setup();
+			try {
+				TestImpl();
+			}
+			finally {
+				Teardown();
+				semaphoreSlim.Release();
+			}
+		}
+
+#if !NET40
+		protected abstract Task TestAsyncImpl();
+
+		[Fact]
+		protected async Task TestAsync() {
+			if (!await semaphoreSlim.WaitAsync(10000).ConfigureAwait(false))
+				Assert.True(false, "Wait failed due to timeout");
+			Setup();
+			try {
+				await TestAsyncImpl().ConfigureAwait(false);
+			}
+			finally {
+				Teardown();
+				semaphoreSlim.Release();
+			}
+		}
+#endif
+
+		private static readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+		protected readonly Context Context = new Context();
+
+		public virtual void Dispose() => Context.Dispose();
+	}
+}
--- TestsCore/UnitTests.cs
+++ TestsCore/UnitTests.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Xunit;
 
 #if EF_CORE
 namespace EntityFrameworkCore.VersionedProperties.Tests {
 #else
 namespace EntityFramework.VersionedProperties.Tests {
 #endif
-	public class UnitTests {
-		[Fact]
-		public void Test() {
+	public class UnitTests : TestBase {
+		protected override void TestImpl() {
+			var person = new Person();
+			Context.People.Add(person);
+			person.FirstName.Value = "Nick";
 
+			var snaps = Context.People.SelectSnapshots(DateTime.UtcNow);
+			//var dt = DateTime.UtcNow;
+			//var f = Context.People.Select(p => {
+			//	p.
+			//	Context.StringVersions.Where(x => x.VersionedId == p.FirstName.Id).OrderByDescending(x => x.Added).First(v => v.Added > dt)
+			//});
+			//var what = Context.People.Join(Context.StringVersions.Last(x => x.Added < dt), p => p.FirstName.Id, sv => sv.VersionedId, (p, version) => { p. });
 		}
+
+#if !NET40
+		protected override Task TestAsyncImpl() {
+			return null;
+		}
+#endif
 	}
 }
