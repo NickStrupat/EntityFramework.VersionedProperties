6521e37a17bf55a3b2ffe554959452de34c2aa49
Nick Strupat <nick.strupat@gmail.com>
2016-11-27 23:10:28 -0500

    hide all the XVersions DbSets from Intellisense unless settings don't filter it

--- EntityFrameworkCore.VersionedProperties/DbContextWithVersionedProperties.cs
+++ EntityFrameworkCore.VersionedProperties/DbContextWithVersionedProperties.cs
@@ -1,4 +1,5 @@
-﻿#if EF_CORE
+﻿using System.ComponentModel;
+#if EF_CORE
 using System;
 using System.Linq;
 using System.Reflection;
@@ -29,35 +30,35 @@ namespace EntityFramework.VersionedProperties {
 			modelBuilder.Entity(versionType).HasIndex(nameof(VersionBase<Object>.Value));
 		}
 #endif
-		public DbSet<BooleanVersion               > BooleanVersions                { get; set; }
-		public DbSet<DateTimeVersion              > DateTimeVersions               { get; set; }
-		public DbSet<DateTimeOffsetVersion        > DateTimeOffsetVersions         { get; set; }
-		public DbSet<DecimalVersion               > DecimalVersions                { get; set; }
-		public DbSet<DoubleVersion                > DoubleVersions                 { get; set; }
-		public DbSet<GuidVersion                  > GuidVersions                   { get; set; }
-		public DbSet<Int32Version                 > Int32Versions                  { get; set; }
-		public DbSet<Int64Version                 > Int64Versions                  { get; set; }
-		public DbSet<NullableBooleanVersion       > NullableBooleanVersions        { get; set; }
-		public DbSet<NullableDateTimeVersion      > NullableDateTimeVersions       { get; set; }
-		public DbSet<NullableDateTimeOffsetVersion> NullableDateTimeOffsetVersions { get; set; }
-		public DbSet<NullableDecimalVersion       > NullableDecimalVersions        { get; set; }
-		public DbSet<NullableDoubleVersion        > NullableDoubleVersions         { get; set; }
-		public DbSet<NullableGuidVersion          > NullableGuidVersions           { get; set; }
-		public DbSet<NullableInt32Version         > NullableInt32Versions          { get; set; }
-		public DbSet<NullableInt64Version         > NullableInt64Versions          { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<BooleanVersion               > BooleanVersions                { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<DateTimeVersion              > DateTimeVersions               { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<DateTimeOffsetVersion        > DateTimeOffsetVersions         { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<DecimalVersion               > DecimalVersions                { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<DoubleVersion                > DoubleVersions                 { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<GuidVersion                  > GuidVersions                   { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<Int32Version                 > Int32Versions                  { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<Int64Version                 > Int64Versions                  { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<NullableBooleanVersion       > NullableBooleanVersions        { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<NullableDateTimeVersion      > NullableDateTimeVersions       { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<NullableDateTimeOffsetVersion> NullableDateTimeOffsetVersions { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<NullableDecimalVersion       > NullableDecimalVersions        { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<NullableDoubleVersion        > NullableDoubleVersions         { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<NullableGuidVersion          > NullableGuidVersions           { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<NullableInt32Version         > NullableInt32Versions          { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<NullableInt64Version         > NullableInt64Versions          { get; set; }
 
 #if !EF_CORE
-		public DbSet<DbGeographyVersion           > DbGeographyVersions            { get; set; }
-		public DbSet<DbGeometryVersion            > DbGeometryVersions             { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<DbGeographyVersion           > DbGeographyVersions            { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<DbGeometryVersion            > DbGeometryVersions             { get; set; }
 #endif
 
-		public DbSet<StringVersion                > StringVersions                 { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<StringVersion                > StringVersions                 { get; set; }
 
 #if !EF_CORE
-		public DbSet<RequiredDbGeographyVersion   > RequiredDbGeographyVersions    { get; set; }
-		public DbSet<RequiredDbGeometryVersion    > RequiredDbGeometryVersions     { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<RequiredDbGeographyVersion   > RequiredDbGeographyVersions    { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<RequiredDbGeometryVersion    > RequiredDbGeometryVersions     { get; set; }
 #endif
 
-		public DbSet<RequiredStringVersion        > RequiredStringVersions         { get; set; }
+		[EditorBrowsable(EditorBrowsableState.Advanced)] public DbSet<RequiredStringVersion        > RequiredStringVersions         { get; set; }
 	}
 }
--- EntityFrameworkCore.VersionedProperties/IDbContextWithVersionedProperties.cs
+++ EntityFrameworkCore.VersionedProperties/IDbContextWithVersionedProperties.cs
@@ -1,4 +1,5 @@
-﻿#if EF_CORE
+﻿using System.ComponentModel;
+#if EF_CORE
 using EntityFrameworkCore.Triggers;
 using Microsoft.EntityFrameworkCore;
 namespace EntityFrameworkCore.VersionedProperties {
@@ -41,30 +42,30 @@ namespace EntityFramework.VersionedProperties {
 		, IRequiredStringVersions
 	{}
 	
-	public interface IBooleanVersions                { DbSet<BooleanVersion               > BooleanVersions                { get; set; } }
-	public interface IDateTimeVersions               { DbSet<DateTimeVersion              > DateTimeVersions               { get; set; } }
-	public interface IDateTimeOffsetVersions         { DbSet<DateTimeOffsetVersion        > DateTimeOffsetVersions         { get; set; } }
-	public interface IDecimalVersions                { DbSet<DecimalVersion               > DecimalVersions                { get; set; } }
-	public interface IDoubleVersions                 { DbSet<DoubleVersion                > DoubleVersions                 { get; set; } }
-	public interface IGuidVersions                   { DbSet<GuidVersion                  > GuidVersions                   { get; set; } }
-	public interface IInt32Versions                  { DbSet<Int32Version                 > Int32Versions                  { get; set; } }
-	public interface IInt64Versions                  { DbSet<Int64Version                 > Int64Versions                  { get; set; } }
-	public interface INullableBooleanVersions        { DbSet<NullableBooleanVersion       > NullableBooleanVersions        { get; set; } }
-	public interface INullableDateTimeVersions       { DbSet<NullableDateTimeVersion      > NullableDateTimeVersions       { get; set; } }
-	public interface INullableDateTimeOffsetVersions { DbSet<NullableDateTimeOffsetVersion> NullableDateTimeOffsetVersions { get; set; } }
-	public interface INullableDecimalVersions        { DbSet<NullableDecimalVersion       > NullableDecimalVersions        { get; set; } }
-	public interface INullableDoubleVersions         { DbSet<NullableDoubleVersion        > NullableDoubleVersions         { get; set; } }
-	public interface INullableGuidVersions           { DbSet<NullableGuidVersion          > NullableGuidVersions           { get; set; } }
-	public interface INullableInt32Versions          { DbSet<NullableInt32Version         > NullableInt32Versions          { get; set; } }
-	public interface INullableInt64Versions          { DbSet<NullableInt64Version         > NullableInt64Versions          { get; set; } }
-	#if !EF_CORE
-	public interface IDbGeographyVersions            { DbSet<DbGeographyVersion           > DbGeographyVersions            { get; set; } }
-	public interface IDbGeometryVersions             { DbSet<DbGeometryVersion            > DbGeometryVersions             { get; set; } }
-#endif
-	public interface IStringVersions                 { DbSet<StringVersion                > StringVersions                 { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IBooleanVersions                { DbSet<BooleanVersion               > BooleanVersions                { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IDateTimeVersions               { DbSet<DateTimeVersion              > DateTimeVersions               { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IDateTimeOffsetVersions         { DbSet<DateTimeOffsetVersion        > DateTimeOffsetVersions         { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IDecimalVersions                { DbSet<DecimalVersion               > DecimalVersions                { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IDoubleVersions                 { DbSet<DoubleVersion                > DoubleVersions                 { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IGuidVersions                   { DbSet<GuidVersion                  > GuidVersions                   { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IInt32Versions                  { DbSet<Int32Version                 > Int32Versions                  { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IInt64Versions                  { DbSet<Int64Version                 > Int64Versions                  { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface INullableBooleanVersions        { DbSet<NullableBooleanVersion       > NullableBooleanVersions        { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface INullableDateTimeVersions       { DbSet<NullableDateTimeVersion      > NullableDateTimeVersions       { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface INullableDateTimeOffsetVersions { DbSet<NullableDateTimeOffsetVersion> NullableDateTimeOffsetVersions { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface INullableDecimalVersions        { DbSet<NullableDecimalVersion       > NullableDecimalVersions        { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface INullableDoubleVersions         { DbSet<NullableDoubleVersion        > NullableDoubleVersions         { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface INullableGuidVersions           { DbSet<NullableGuidVersion          > NullableGuidVersions           { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface INullableInt32Versions          { DbSet<NullableInt32Version         > NullableInt32Versions          { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface INullableInt64Versions          { DbSet<NullableInt64Version         > NullableInt64Versions          { get; set; } }
 #if !EF_CORE
-	public interface IRequiredDbGeographyVersions    { DbSet<RequiredDbGeographyVersion   > RequiredDbGeographyVersions    { get; set; } }
-	public interface IRequiredDbGeometryVersions     { DbSet<RequiredDbGeometryVersion    > RequiredDbGeometryVersions     { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IDbGeographyVersions            { DbSet<DbGeographyVersion           > DbGeographyVersions            { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IDbGeometryVersions             { DbSet<DbGeometryVersion            > DbGeometryVersions             { get; set; } }
 #endif
-	public interface IRequiredStringVersions         { DbSet<RequiredStringVersion        > RequiredStringVersions         { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IStringVersions                 { DbSet<StringVersion                > StringVersions                 { get; set; } }
+#if !EF_CORE
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IRequiredDbGeographyVersions    { DbSet<RequiredDbGeographyVersion   > RequiredDbGeographyVersions    { get; set; } }
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IRequiredDbGeometryVersions     { DbSet<RequiredDbGeometryVersion    > RequiredDbGeometryVersions     { get; set; } }
+#endif
+	[EditorBrowsable(EditorBrowsableState.Advanced)] public interface IRequiredStringVersions         { DbSet<RequiredStringVersion        > RequiredStringVersions         { get; set; } }
 }
