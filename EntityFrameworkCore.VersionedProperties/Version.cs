1c9662fcd51737e27ba29ab4fd16a6803543ce69
Nick Strupat <nick.strupat@gmail.com>
2016-12-05 00:49:51 -0500

    add equality implementations

--- EntityFrameworkCore.VersionedProperties/Version.cs
+++ EntityFrameworkCore.VersionedProperties/Version.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
+using System.Reflection;
 
 #if EF_CORE
 namespace EntityFrameworkCore.VersionedProperties {
@@ -18,7 +20,7 @@ namespace EntityFramework.VersionedProperties {
 	interface IVersion {}
 
 	[DebuggerDisplay("Value = {Value}")]
-	public abstract class VersionBase<TValue> : IVersion {
+	public abstract class VersionBase<TValue> : IVersion, IEquatable<VersionBase<TValue>> {
 		/// <summary>Gets the unique identifier for this version of the data</summary>
 		public Int64 Id { get; internal set; }
 
@@ -36,6 +38,37 @@ namespace EntityFramework.VersionedProperties {
 		public TValue Value { get; internal set; }
 
 		public override String ToString() => Value?.ToString() ?? String.Empty;
+
+		#region Equality
+		public override Boolean Equals(Object obj) => Equals(obj as VersionBase<TValue>);
+
+		public Boolean Equals(VersionBase<TValue> version) {
+			if (ReferenceEquals(version, null))
+				return false;
+			if (ReferenceEquals(version, this))
+				return true;
+			return Id == version.Id
+			    && VersionedId == version.VersionedId
+			    && Added == version.Added
+			    && ValueEqualityComparer.Equals(Value, version.Value);
+		}
+
+		public override Int32 GetHashCode() {
+			var hash = 1374496523;
+			unchecked {
+				hash = (hash * -1521134295) + Id.GetHashCode();
+				hash = (hash * -1521134295) + VersionedId.GetHashCode();
+				hash = (hash * -1521134295) + Added.GetHashCode();
+				if (IsTValueAValueType || !ReferenceEquals(Value, null))
+					hash = (hash * -1521134295) + ValueEqualityComparer.GetHashCode(Value);
+			}
+			return hash;
+		}
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		private static readonly Boolean IsTValueAValueType = typeof(TValue).GetTypeInfo().IsValueType;
+
+		protected virtual IEqualityComparer<TValue> ValueEqualityComparer => EqualityComparer<TValue>.Default;
+		#endregion
 	}
 
 	public abstract class RequiredValueVersionBase<TValue> : VersionBase<TValue>
